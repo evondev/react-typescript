3fb184fdf6390addf2be65c93eb103b48b91a8c2
evondev <trananhtuan400@gmail.com>
2022-08-10 00:28:56 +0700

    functions extra

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -41,3 +41,56 @@ function parseCoordinate(arg1: any, arg2?: any): Coordinate {
 // Type assertion `as`
 parseCoordinate({ x: 10, y: 20 });
 parseCoordinate(10, 20);
+// Normal function
+function addNumbers(a: number, b: number): number {
+  return a + b;
+}
+// Arrow function
+const addStrings = (x: string, y: string): string => {
+  return `${x} ${y}`;
+};
+// Default parameters
+function addNumbersWithDefaultParams(a: number = 10, b: number = 20): number {
+  return a + b;
+}
+addNumbersWithDefaultParams(); // 30
+// Union types
+function format(
+  title: string,
+  description: string,
+  amount: string | number
+): string {
+  return `${title} ${description} ${amount}`;
+}
+format("evondev", "developer", 50);
+format("evondev", "developer", "50");
+// Void functions
+function contact(email: string, phone: number): void {
+  console.log(email, phone);
+}
+// Promise functions
+const fetchData = (url: string): Promise<string> =>
+  Promise.resolve(`Get data from ${url}`);
+// Rest parameters
+function information(id: number, ...names: string[]): string {
+  return `${id} ${names.join(" ")}`;
+}
+information(1, "evondev", "tuan"); // ["evondev", "tuan"]
+// With callback
+function handleFile(text: string, callback: () => void): void {
+  console.log(text);
+  callback();
+}
+// Function params with params
+// Function as types
+type UpdateArray = (n: number) => number;
+function handleUpdateArray(numbers: number[], update: UpdateArray): number[] {
+  return numbers.map((item) => update(item));
+}
+handleUpdateArray([1, 2, 3, 4, 5], (n) => n * 5); // 5 10 15 20 25
+// Function return function
+function handleValue(val: number): (n: number) => number {
+  return (n: number): number => n * val;
+}
+const value = handleValue(5);
+console.log(value(10)); // 50
