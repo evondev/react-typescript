1041e19ffffd11ed130c6564e8333c228ce30d24
evondev <trananhtuan400@gmail.com>
2022-09-15 00:06:22 +0700

    todo app with use reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,124 +1,82 @@
-import { useEffect, useState } from "react";
-import Card from "./components/Card";
-import { Permission } from "./utils/enums";
-import { simpleUseState } from "./utils/tuple";
-import { Age } from "./utils/types";
-// arrObj: {}[] = [{}]
-const reviews: {
-  name: string;
-  image: string;
-  stars: number;
-  premiumUser: boolean;
-  date: string | number;
-}[] = [
-  {
-    name: "Evondev",
-    image: "",
-    stars: 5,
-    premiumUser: true,
-    date: "05/09/2022",
-  },
-  {
-    name: "CharkaUI",
-    image: "",
-    stars: 4,
-    premiumUser: false,
-    date: 123456789,
-  },
-];
-
-const travelItem: {
-  image: string;
-  name: string;
-  totalReviews: number;
-  rating: number;
-  location: string;
-  price: number;
-  date: string;
-  departure: string;
-  features: {
-    freeWifi: boolean;
-    freeParking: boolean;
-    specialOffer: boolean;
-  };
-}[] = [
-  {
-    image: "https://source.unsplash.com/random",
-    name: "Zuich, Switzerland",
-    totalReviews: 148,
-    rating: 4.8,
-    location: "Switzerland",
-    price: 300,
-    date: "05/09/1994",
-    departure: "Brazil",
-    features: {
-      freeWifi: true,
-      freeParking: true,
-      specialOffer: false,
-    },
-  },
-];
-
-// const obj: {} = {}
-// union types |
-// Permissions
-function App() {
-  const [count, setCount] = useState(0);
-  const user: {
-    firstName: string;
-    lastName: string;
-    age: Age;
-    isStudent: boolean;
-    school: (string | number)[];
-    scores: number[];
-    contact: [number, string];
-    permission: Permission;
-  } = {
-    firstName: "Tran",
-    lastName: "Anh Tuan",
-    age: "40",
-    isStudent: false,
-    school: ["Cao Thang", "Ton Duc Thang", "Sai Gon University", 35],
-    scores: [10, 9, 8],
-    contact: [123456789, "tuan@gmail.com"],
-    permission: Permission.ADMIN,
-  };
-  function displayReview(
-    totalReviews: number | string,
-    name: string,
-    premium: boolean
-  ) {
-    return (
-      <>
-        Review total <strong>{totalReviews}</strong> | Last reviewed by{" "}
-        <strong>{name}</strong> {premium ? "⭐️" : ""}
-      </>
-    );
+import React, { useReducer, useRef } from "react";
+const Heading = ({ title }: { title: string }) => {
+  return <h2 className="font-primary font-bold text-2xl mb-5">{title}</h2>;
+};
+type ActionType =
+  | { type: "ADD"; text: string }
+  | { type: "REMOVE"; id: number };
+interface Todo {
+  id: number;
+  text: string;
+}
+const todoReducer = (state: Todo[], action: ActionType) => {
+  switch (action.type) {
+    case "ADD":
+      return [
+        ...state,
+        {
+          id: state.length,
+          text: action.text,
+        },
+      ];
+    case "REMOVE":
+      return state.filter((todo: Todo) => todo.id !== action.id);
+    default:
+      throw new Error("");
   }
-  // optional: không bắt buộc
-  // name?: parameter này không bắt buộc
-  // const travelItem: image, name, totalReviews, rating, location, price, date, departure, features: wifi, parking, offer
-  // Khai báo 1 biến có tên là travelItem là một mảng chứa các object có các properties(thuộc tính) như trên
+};
+const initialState: Todo[] = [];
+const App = () => {
+  const [todos, dispatch] = useReducer(todoReducer, initialState);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const onRemoveTodo = (todoId: number) => {
+    dispatch({
+      type: "REMOVE",
+      id: todoId,
+    });
+  };
+  const onAddTodo = () => {
+    if (inputRef.current) {
+      dispatch({
+        type: "ADD",
+        text: inputRef.current.value,
+      });
+      inputRef.current.value = "";
+    }
+  };
   return (
     <div>
-      {/* <Card ></Card> */}
-      <div className="review">
-        <div className="review-image">
-          <img
-            src="https://images.unsplash.com/photo-1659464797962-62a9b8324abc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-            alt=""
-          />
+      <Heading title="Todo App"></Heading>
+      <div className="max-w-sm">
+        <div className="mb-5 flex flex-col gap-y-5">
+          {todos.map((todo) => (
+            <div className="flex items-center gap-x-3" key={todo.id}>
+              <span>{todo.text}</span>
+              <button
+                onClick={() => onRemoveTodo(todo.id)}
+                className="p-2 rounded-lg bg-red-500 text-white font-medium text-sm"
+              >
+                Remove
+              </button>
+            </div>
+          ))}
         </div>
-        <div className="review-info">
-          {displayReview(
-            reviews.length,
-            reviews[0].name,
-            reviews[0].premiumUser
-          )}
+        <div className="flex items-center gap-x-5">
+          <input
+            type="text"
+            className="p-4 border border-slate-200 rounded-lg outline-none"
+            ref={inputRef}
+          />
+          <button
+            onClick={onAddTodo}
+            className="p-4 rounded-lg bg-blue-500 text-white text-center"
+          >
+            Add todo
+          </button>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
