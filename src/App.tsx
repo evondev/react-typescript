e4cf3d2a7efee414165bea86504005542acc6413
evondev <trananhtuan400@gmail.com>
2022-09-17 00:22:29 +0700

    htmldetailsprops

--- src/App.tsx
+++ src/App.tsx
@@ -42,6 +42,7 @@ const App = () => {
       </Boxed>
       <div className="max-w-sm">
         <RenderList
+          keyExtractor={(todo) => todo.id}
           items={todos}
           render={(todo) => (
             <div className="flex items-center gap-x-3">
@@ -57,6 +58,7 @@ const App = () => {
         ></RenderList>
         <RenderList
           items={products}
+          keyExtractor={(product) => product.id}
           render={(product) => <div>{JSON.stringify(product)}</div>}
         ></RenderList>
         <div className="flex items-center gap-x-5">
@@ -73,6 +75,22 @@ const App = () => {
           </button>
         </div>
       </div>
+      <Button
+        className="text-white text-sm p-4 rounded-xl bg-blue-500"
+        type="button"
+        disabled
+      >
+        Buy now
+      </Button>
+      <Input
+        placeholder="Enter your name"
+        className="p-3 border border-slate-200 rounded-lg"
+      ></Input>
+      {/* <View as='h2'>This is heading2</View> */}
+      {/* <h2>This is heading2</h2> */}
+      <View as="h2" className="capitalize text-2xl font-medium my-5">
+        This is a heading2
+      </View>
     </div>
   );
 };
@@ -102,11 +120,49 @@ const Boxed = ({ children }: { children?: React.ReactNode }) => {
 const RenderList = <T,>({
   items,
   render,
+  keyExtractor,
 }: {
   items: T[];
   render: (item: T) => React.ReactNode;
+  keyExtractor: (item: T) => string | number;
 }) => {
-  return <>{items.map((item) => render(item))}</>;
+  return (
+    <ul>
+      {items.map((item, index) => (
+        <li key={keyExtractor(item)}>{render(item)}</li>
+      ))}
+    </ul>
+  );
+};
+type ButtonProps = {
+  children: React.ReactNode;
+} & React.DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>;
+const Button = ({ children, ...rest }: ButtonProps) => {
+  return <button {...rest}>{children}</button>;
+};
+type InputProps = React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>;
+const Input = (rest: InputProps) => {
+  return <input {...rest} />;
+};
+// IntrinsicElements key: button
+// IntrinsicElements type: React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>;
+// obj.name, obj['name'] => IntrinsicElements['button']
+type ViewProps<T extends keyof JSX.IntrinsicElements> = {
+  children: React.ReactNode;
+  as: T;
+} & JSX.IntrinsicElements[T];
+const View = <T extends keyof JSX.IntrinsicElements>({
+  children,
+  as,
+  ...rest
+}: ViewProps<T>) => {
+  return React.createElement(as, { ...rest }, children);
 };
 
 export default App;
